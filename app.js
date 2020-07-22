e58815a3e084732b7d147246300de3b619d6e9ce
Jonathan <jonathanvouilloz@gmail.com>
2020-07-22 11:44:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+    res.send('Hello World')
+})
+
+server.listen(8080, function(){
+    console.log(`app listening on ${server.address().port}`);
+})
